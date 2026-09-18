3eccb4c7b24515bc53745ada4172ae8f1e5cedf9
Tanveer Bhamra <f20251830@pilani.bits-pilani.ac.in>
2026-09-18 13:45:06 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -3,10 +3,21 @@
 
 module tb;
 
+  parameter TB_WIDTH = 8;
+  parameter TB_DEPTH = 4;
+
   // TODO: declare the inputs and outputs
-
+  reg [$clog2(TB_DEPTH)-1:0] t_sel;
+  wire [TB_WIDTH-1:0]         t_dout;
   // TODO: instantiate DUT here
-
+  lut #(
+    .WIDTH(TB_WIDTH),
+    .DEPTH(TB_DEPTH)
+  )
+  DUT(
+    .sel(t_sel),
+    .dout(t_dout)
+  );
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
   initial begin
@@ -16,12 +27,17 @@ module tb;
     end
   end
 
+  integer i;
   initial begin
-    // TODO: apply different input combinations
-
+  // TODO: apply different input combinations
+  for(i = 0; i < TB_DEPTH; i = i + 1) begin
+    t_sel = i; 
+    #5;
+  end
+  $finish;
   end
 
-  initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
-
+  initial begin
+  $monitor($time, " sel=%d | dout=%d", t_sel, t_dout); // change as required
+  end
 endmodule
